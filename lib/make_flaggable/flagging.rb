0ec013817a8f0381467b1790e8ff2ca22371e19b
Raj Rathore <rajr4thore@gmail.com>
2014-05-04 13:20:58 +0530

    added mass assignment to flagging class

--- lib/make_flaggable/flagging.rb
+++ lib/make_flaggable/flagging.rb
@@ -2,5 +2,7 @@ module MakeFlaggable
   class Flagging < ActiveRecord::Base
     belongs_to :flaggable, :polymorphic => true
     belongs_to :flagger, :polymorphic => true
+    
+    attr_accessible :flaggable, :flagger, :reason
   end
 end
