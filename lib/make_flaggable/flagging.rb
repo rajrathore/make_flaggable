d7f49353bfc5cd0ffc2c0995cdcfde83174a42ad
Raj Rathore <rajr4thore@gmail.com>
2014-05-05 10:39:44 +0530

    scope iwth_the_flag added

--- lib/make_flaggable/flagging.rb
+++ lib/make_flaggable/flagging.rb
@@ -4,5 +4,6 @@ module MakeFlaggable
     belongs_to :flagger, :polymorphic => true
     
     attr_accessible :flaggable, :flagger, :reason
+    scope :with_the_flag, lambda { |flag| where(:reason => flag.to_s) }
   end
 end
