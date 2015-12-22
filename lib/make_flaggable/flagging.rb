fde11161ea8470c4655356bc2f0fc34186363504
Raj Rathore <rajr4thore@gmail.com>
2015-12-22 15:14:47 +0530

    spec_helper updated

--- lib/make_flaggable/flagging.rb
+++ lib/make_flaggable/flagging.rb
@@ -2,8 +2,8 @@ module MakeFlaggable
   class Flagging < ActiveRecord::Base
     belongs_to :flaggable, :polymorphic => true
     belongs_to :flagger, :polymorphic => true
-    
-    attr_accessible :flaggable, :flagger, :reason
+
+    # attr_accessible :flaggable, :flagger, :reason
     scope :with_the_flag, lambda { |flag| where(:reason => flag.to_s) }
   end
 end
